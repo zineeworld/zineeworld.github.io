143dd5fe3183d699d6508ca4cec58c2714f68ca0
zinee <zineeworld@gmail.com>
2019-04-08 13:42:32 +0900

    add a mmt-donut-chart.js

--- demo/mmt-donut-chart/assets/shBrushXml.js
+++ demo/mmt-donut-chart/assets/shBrushXml.js
@@ -17,7 +17,7 @@
 ;(function()
 {
 	// CommonJS
-	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+	typeof(require) != 'undefined' ? SyntaxHighlighter = require('demo/mmt-donut-chart/assets/shCore').SyntaxHighlighter : null;
 
 	function Brush()
 	{
