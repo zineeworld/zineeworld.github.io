089acdd5ff30c6d5b2a2f428d425016ba0c06a23
zinee <zineeworld@gamil.com>
2024-01-25 23:32:41 +0900

    ing....

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -8,11 +8,8 @@ const Information = ({ information }: Pick<DataProps, "information">) => {
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-2">
         <h1 className="leading-[1.15]">
-          {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
-          안녕하세요,
-          <br /> 프론트엔드 개발자{" "}
-          <span className="text-PRIMARY font-semibold">{information.name}</span>
-          입니다.
+          안녕하세요,<br />
+          UI 개발자 <span className="text-PRIMARY font-semibold">{information.name}</span>입니다.
         </h1>
         <div className="flex gap-1">
           {information.contact.map((contact) => (
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,22 +23,23 @@ module.exports = {
         /**
          * @description 자기소개의 이름 부분, code tag의 darkmode에 사용되는 컬러입니다.
          */
-        PRIMARY: "#12b886",
+        PRIMARY: "#C8D444",
 
         /**
          * @description code tag, link hover icon에 사용되는 컬러입니다.
          */
-        PRIMARY_HEAVY: "#087f5b",
+        PRIMARY_HEAVY: "#45A0DB",
 
         /**
          * @description 페이지 최상단 gradient의 시작 컬러입니다.
          */
-        GRADIENT_FROM: "#51cf66",
+        GRADIENT_FROM: "#122A47",
 
+        GRADIENT_VIA: "#45A0DB",
         /**
          * @description 페이지 최상단 gradient의 종료 컬러입니다.
          */
-        GRADIENT_TO: "#0c8599",
+        GRADIENT_TO: "#C8D444",
       },
     },
   },
