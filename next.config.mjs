d932408c14aad2c2443040acb09e09d3b345c7a2
11283 <zinee@finnq.com>
2024-07-03 14:59:15 +0900

    init

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '', // 여기에 GitHub Pages 저장소 이름을 입력하세요.
+  assetPrefix: '',
+};
+
+export default nextConfig;
