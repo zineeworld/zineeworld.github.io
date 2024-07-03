bd64ee4b5f83145d5321c7d2b4787c185d504019
11283 <zinee@finnq.com>
2024-07-03 16:32:38 +0900

    add sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+import path from 'path';
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const nextConfig = {
   output: 'export',
   basePath: '', // 여기에 GitHub Pages 저장소 이름을 입력하세요.
   assetPrefix: '',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./styles/main.scss";
 
 
 export const metadata: Metadata = {
@@ -13,8 +13,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>zineeworld</title>
+      </head>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
