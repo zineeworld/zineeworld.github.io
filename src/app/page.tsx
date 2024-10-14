e4bd36de172aaf9b4ec7b0a0fe0d308e00aa4f0d
11283 <zinee@finnq.com>
2024-10-14 13:57:41 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,13 @@ export default function Home() {
   return (
     <div className="ui-home">
       <div className="box">
-        <Link href="/resume">res</Link>
+        <a href="/" className="logo">Zineeworld</a>
+        <Link href="/resume" className="resume">
+          <span className="category">ABOUT</span>
+          <span className="title">타이틀 문장</span>
+          <span className="desc">11년차 마크업 개발자</span>
+          <span className="date">2024. 09.</span>
+        </Link>
       </div>
 
       <ul className="pf-list">
@@ -14,7 +20,7 @@ export default function Home() {
         <li><Link href="/portfolio/coinone">c</Link></li>
         <li><Link href="/portfolio/kakaoenterprise">k</Link></li>
         <li><Link href="/portfolio/finnq">f</Link></li>
-        <li><Link href="/portfolio/next">n?</Link></li>
+        <li><Link href="/portfolio/next">next?</Link></li>
       </ul>
     </div>
   );
